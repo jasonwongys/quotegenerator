7153e494b6f3cb07a246fde5b999e0b7905f48c2
Wong Yat Sing Jason  (NCS) <yat.sing.jason.wong@ncs.com.sg>
2019-08-20 16:33:22 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      randomQuote: ""
+    }
+
+    this.ask = this.ask.bind(this);
+  }
+
+  ask() {
+    this.setState({
+      randomQuote: Math.floor(Math.random() * 6) + 1 
+    });
+  }
+
+  render() {
+
+    const quotes = ["There will be a day when I no longer can do this. THAT DAY IS NOT TODAY.",
+    "Your friends should motivate and inspire you. Your circle should be well rounded and supportive. Keep it tight. Quality over quantity, always.",
+    "Don’t make change too complicated. Just begin.",
+    "YOU are the creator of your own destiny.",
+    "This is the start of something beautiful.",
+    "Don’t let small minds convince you that your dreams are too big.",
+  "The temptation to quit will be greatest just before you are about to succeed."];
+    const genQuote = quotes[this.state.randomQuote];
+
+    return (
+      <div className="container">
+      <h1>Motivational Quotes</h1>
+        <h2 className="quote-card">{genQuote}</h2>
+        <button onClick={this.ask}>Generate new quote</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
